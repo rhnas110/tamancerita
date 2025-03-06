c79852cced1f8b2e0de95d8b39ca7c8e371bd187
rhnas110 <raihanas110@gmail.com>
2025-03-06 21:08:09 +0700

    feat: about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/home";
+import { Aboutpage } from "./pages/about";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<Aboutpage />} />
       <Route path="/search" element={<h1>Search page</h1>} />
       <Route path="/detail/:id" element={<h1>Detail page</h1>} />
     </Routes>
